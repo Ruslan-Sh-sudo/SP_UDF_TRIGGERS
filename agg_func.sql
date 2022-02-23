69d86c90c699514dd5fc3ba8060eb701516eabc9
Ruslan-Sh-sudo <ruslan2411sh@gmail.com>
2022-02-23 14:12:11 +0200

    added description to tasks in agg_funcs

--- agg_func.sql
+++ agg_func.sql
@@ -1,12 +1,31 @@
+  /*
+  Выбрать названия и количество групп из таблицы HumanResources.Department
+  */
+
   SELECT groupname, COUNT(*) AS quantity
     FROM humanresources.department
 GROUP BY groupname;
+
+ /*
+  Найти максимальную ставку для каждого сотрудника из таблиц HumanResources.EmployeePayHistory, 
+  HumanResources.Employee
+ */
 
   SELECT e.jobtitle, MAX(ep.rate) AS highest_rate
     FROM humanresources.employee e
     JOIN humanresources.employeepayhistory ep
 	    ON e.businessentityid = ep.businessentityid
 GROUP BY e.jobtitle;
+
+ /*
+  Выбрать минимальную цену единицы товара по подкатегориям 
+  (названия из таблицы PRODUCTION.PRODUCTSUBCATEGORY, минимальная цена из таблицы SALES.SALESORDERDETAIL) 
+  используя таблицы:
+  Sales.SalesOrderHeader, 
+  Sales.SalesOrderDetail, 
+  Production.Product, 
+  Production.ProductSubcategory
+ */
 
    SELECT psub.name, MIN(unitprice) AS min_group_price
      FROM (
@@ -19,11 +38,27 @@ GROUP BY e.jobtitle;
        ON psub.productsubcategoryid = pricelist.productsubcategoryid
  GROUP BY psub.name;
 
+ /*
+  Вычислить название и количество подкатегорий товара в каждой категории, 
+  используя таблицы: 
+  Production.ProductCategory, 
+  Production.ProductSubcategory
+ */
+
   SELECT psub.name, COUNT(pr.name) AS subcategories
     FROM production.productsubcategory psub
     JOIN production.product pr
       ON psub.productsubcategoryid = pr.productsubcategoryid
 GROUP BY psub.name;
+
+ /*
+  Вывести среднюю сумму заказа по подкатегориям товаров, 
+  используя таблицы: 
+  Sales.SalesOrderHeader, 
+  Sales.SalesOrderDetail, 
+  Production.Product, 
+  Production.ProductSubcategory
+ */
 
   SELECT psub.name, AVG(totaldue) AS avg_subcat_sum
     FROM (SELECT name, totaldue, productsubcategoryid
@@ -38,6 +73,11 @@ GROUP BY psub.name;
     JOIN production.productsubcategory psub
       ON psub.productsubcategoryid = totaldue_subcat_id.productsubcategoryid
 GROUP BY psub.name;
+
+ /*
+ Найти ID сотрудника с максимальным рейтом и дату назначения рейта, 
+ используя таблицу HumanResources.EmployeePayHistory
+ */
 
 SELECT businessentityid, ratechangedate 
   FROM HumanResources.EmployeePayHistory
